68883ceabef5825d4c0d231060da63efcbf1d2db
Yann Achard <achard.y@gmail.com>
2015-03-15 15:23:09 +1100

    Colored explosions

--- MissileCommandUnity/Assets/scripts/Detonation.cs
+++ MissileCommandUnity/Assets/scripts/Detonation.cs
@@ -1,25 +1,51 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 
 public class Detonation : MonoBehaviour {
 
-	private Material mat;
+	public float duration = 5.0f;
 
+	private float timeSeed;
+	private float timeLeft;
 	private static float lastSoundTime = -42.0f;
+	private MeshRenderer rend;
+	private Color[] c = new Color[]{
+		Color.magenta, // new Vector4(0.8f, 0.8f, 0.1f, 1),
+		Color.cyan, // new Vector4(0.1f, 0.8f, 0.8f, 1),
+		Color.yellow, // new Vector4(0.1f, 0.8f, 0.8f, 1),
+		Color.white, // new Vector4(0.1f, 0.8f, 0.8f, 1),
+		Color.red, // new Vector4(1.0f, 0.1f, 0.1f, 1),
+		//Color.` new Vector4(0.1f, 1.0f, 0.1f, 1),
+	};
 
 	void Start () {
 		transform.eulerAngles = new Vector3(90, 0, 0);
 		GetComponent<Animator>().SetTrigger("StartTrigger");
 
-		float time = Time.realtimeSinceStartup;
-		if (time - lastSoundTime >= 0.2f) { 
+		float time = Random.Range(0 , 1000.0f);
+		if (time - lastSoundTime >= 0.2f) {
 			GetComponent<AudioSource>().Play();
 			lastSoundTime = time;
 		}
-		Destroy(gameObject, 5.0f);
+		timeLeft = duration;
+		timeSeed = time;
+
+		rend = GetComponent<MeshRenderer>();
+//		Material mat = new Material(rend.material);
+//		rend.material = mat;
+	
+		Destroy(gameObject, duration);
+		//Destroy(mat, duration);
 	}
 	
 	void Update () {
+		timeLeft -= Time.deltaTime;
+		int t = (int)(timeSeed + Time.realtimeSinceStartup * 1000.0f);
+		int u = (t/50) % c.Length;
+		int f = t % 50;
+		Color col = Color.Lerp(c[u], c[(u+1) % c.Length], f / 50.0f);
+		col.a = Mathf.Clamp01(timeLeft-1.2f);
+		rend.material.color = col;
 	}
 
 	void OnTriggerEnter2D(Collider2D coll)
